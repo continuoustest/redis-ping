6062dc47ef014ffa671e8fa7d524ab1b71f91987
chris-continuous <59437559+chris-continuous@users.noreply.github.com>
2020-01-09 17:39:34 +0100

    Update redis-test.php

--- redis-test.php
+++ redis-test.php
@@ -5,12 +5,14 @@ $redis = new Redis();
 $host  = getenv('REDIS_HOST');
 $port  = getenv('REDIS_PORT');
 //Connecting to Redis
-echo 'connect to redis : '. $host . ':' . $port; 
+echo 'connect to redis : '. $host . ':' . $port . PHP_EOL; 
+echo "PING" . PHP_EOL;
 $redis->connect( $host, $port);
 
 if ($redis->ping()) {
-        echo "PONG\n";
+        echo "PONG" . PHP_EOL;
         exit(0);
 }
+echo "PLOOF" . PHP_EOL;
  exit(12);
 ?>
